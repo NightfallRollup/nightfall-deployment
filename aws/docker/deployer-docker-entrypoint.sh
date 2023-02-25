a323e8bba3dce130949de4f7ea7ae0e502fac58c
David <druiz0992@gmail.com>
2023-02-25 12:47:03 +0100

    fix: merge with 7d6b132845013246b40dd2b6f067246eb1e6f451

--- aws/docker/deployer-docker-entrypoint.sh
+++ aws/docker/deployer-docker-entrypoint.sh
@@ -2,26 +2,7 @@
 set -o errexit
 set -o pipefail
 
-if [ -z "${ETH_PRIVATE_KEY}" ]; then
-  while true; do
-    echo "Waiting for connection with ${BLOCKCHAIN_WS_HOST}..."
-    WEB3_RESPONSE=$(curl -f --write-out '%{http_code}' --silent --output \
-    --location --request POST https://"${BLOCKCHAIN_WS_HOST}" \
-    --header 'Content-Type: application/json' \
-    --data-raw '{
-       "jsonrpc":"2.0",
-       "method":"eth_blockNumber",
-       "params":[],
-       "id":83
-     }') 
-
-    if [ "${WEB3_RESPONSE}" -ge "200" ] && [ "${WEB3_RESPONSE}" -le "499" ]; then
-        echo "Connect to ${BLOCKCHAIN_WS_HOST}..." 
-	      break
-    fi
-  sleep 10
- done
-fi
+while ! nc -z ${BLOCKCHAIN_WS_HOST} 80; do sleep 3; done
 
 if [[ "${SKIP_DEPLOYMENT}" != "true" && "${PARALLEL_SETUP}" == "false" ]]; then
   echo "PARALLEL SETUP DISABLED...."
